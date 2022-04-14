5c671b2bfded7dabf4e7765001478bdfc66544c1
Nekiro <nekiro.dev@gmail.com>
2022-04-14 17:46:32 +0200

    Fix morgan

--- server/users/src/app.js
+++ server/users/src/app.js
@@ -11,7 +11,7 @@ import authRouter from './routes/auth';
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
