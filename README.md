7394d2d85f7e402348847d4159c5ded869795cc8
Nekiro <nekiro.dev@gmail.com>
2022-04-28 13:57:34 +0200

    Add github actions

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repository implements API for [job-application-tracker](https://github.com/
 Mentioned project is front end app written in React.
 
 API is being implemented following monolithic architecture.
-Hosted using serverless at: [AWS Lambda](https://c62762a6q9.execute-api.eu-central-1.amazonaws.com)
+Hosted using express at: [Oracle VPS](http://130.61.52.213:3000)
 
 Documentation:
 https://documenter.getpostman.com/view/9956320/UyrBhvHX#ff941169-a0e5-4f1f-9d83-f4b2896d50a2
--- test/mocks/db.js
+++ test/mocks/db.js
@@ -4,35 +4,17 @@ import prismaClient from '../../src/database';
 
 export const createDatabase = async () => {
   // initiate new client
-
-  // await exec(
-  //   'npx prisma migrate reset --force --schema ./test/prisma/schema.prisma'
-  // );
-
-  // await exec('npx prisma db push --schema ./test/prisma/schema.prisma');
-
   await prismaClient.$connect();
+};
 
-  // const companies = await prisma.Company.findMany({
-  //   where: { userId: user.id },
-  // });
-
+export const destroyDatabase = async () => {
   // wipe all
   // this a very hacky way using (probably) temporary (risky) fields
   for (const model of prismaClient._dmmf.datamodel.models) {
     const modelRef = prismaClient[model.name];
-
-    // modelRef.findMany = (args) => {
-
-    // }
-
     await modelRef.deleteMany();
-
-    //console.log('Wiping model: ' + model.name);
   }
-};
 
-export const destroyDatabase = async () => {
   await prismaClient.$disconnect();
 };
 
