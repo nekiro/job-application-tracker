24776cc96b3538bf080b1fc723f9cc74dcc2fab1
Nekiro <nekiro.dev@gmail.com>
2022-06-07 14:42:50 +0200

    Add home html page with documentation url

--- src/app.ts
+++ src/app.ts
@@ -36,6 +36,27 @@ app.use('/companies', companiesRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 
+// home route
+const homeHtml = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>API Home</title>
+  </head>
+  <body>
+    <h1>Job Application Tracker API</h1>
+    <h2><a href="${process.env.DOCUMENTATION_URL}" target="_blank">Documentation</a></h2>
+  </body>
+</html>
+`;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send(homeHtml);
+});
+
 // error handler
 app.use(errorHandler);
 
