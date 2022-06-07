d722b356ba33cf0be22ee7c05a63a4df1003dd65
Nekiro <nekiro.dev@gmail.com>
2022-06-07 14:42:31 +0200

    Fix auth tests

--- __tests__/auth/sign-in.test.ts
+++ __tests__/auth/sign-in.test.ts
@@ -1,7 +1,7 @@
 import { prismaMock } from '../../src/singleton';
 import { getMockReq, getMockRes } from '@jest-mock/express';
 import { signIn } from '../../src/handlers/auth.handler';
-import { AuthError } from '../../src/middlewares/errorHandler';
+import AuthError from '../../src/errors/AuthError';
 
 jest.mock('../../src/util/crypt', () => {
   return {
@@ -18,60 +18,6 @@ jest.mock('../../src/util/authentication', () => {
 });
 
 describe('Sign-in', () => {
-  describe('given no payload', () => {
-    test('should call next with error', async () => {
-      //@ts-ignore
-      prismaMock.user.findFirst.mockResolvedValue(null);
-
-      const req = getMockReq();
-      const { res, next } = getMockRes();
-
-      await signIn(req, res, next);
-
-      expect(prismaMock.user.findFirst).toHaveBeenCalled();
-
-      expect(next).toHaveBeenCalledWith(
-        new AuthError("Email or password doesn't match")
-      );
-    });
-  });
-
-  describe('given valid payload', () => {
-    test('should respond with valid token and user object', async () => {
-      const mockedUser: any = {
-        id: 'foo',
-        email: 'foo@bar',
-      };
-
-      prismaMock.user.findFirst.mockResolvedValue({
-        ...mockedUser,
-        tokenSecret: 'foobar',
-      });
-
-      const req = getMockReq({
-        body: { email: mockedUser.email, password: 'foo' },
-      });
-
-      const { res, next } = getMockRes();
-
-      await signIn(req, res, next);
-
-      expect(prismaMock.user.findFirst).toHaveBeenCalledWith({
-        where: { email: mockedUser.email },
-      });
-
-      expect(res.json).toHaveBeenCalledWith(
-        expect.objectContaining({
-          token: expect.any(String),
-          expiresAt: expect.any(Number),
-          user: expect.objectContaining(mockedUser),
-        })
-      );
-    });
-  });
-});
-
-describe('Sign-up', () => {
   describe('given no payload', () => {
     test('should call next with error', async () => {
       //@ts-ignore
---
+++ __tests__/auth/sign-up.test.ts
@@ -0,0 +1,64 @@
+import { prismaMock } from '../../src/singleton';
+import { getMockReq, getMockRes } from '@jest-mock/express';
+import { signUp } from '../../src/handlers/auth.handler';
+import { Role } from '../../src/middlewares/role';
+import ResourceExistsError from '../../src/errors/ResourceExistsError';
+
+describe('Sign-up', () => {
+  describe('given valid payload', () => {
+    test('should respond with created user', async () => {
+      const mockedUser: any = {
+        email: 'foo@bar',
+        firstName: 'foo',
+        lastName: 'bar',
+        password: 'foobar',
+        role: Role.USER,
+      };
+
+      prismaMock.user.create.mockResolvedValue({
+        ...mockedUser,
+        tokenSecret: 'foobar',
+      });
+
+      const req = getMockReq({
+        body: mockedUser,
+      });
+      const { res, next } = getMockRes();
+
+      await signUp(req, res, next);
+
+      expect(prismaMock.user.create).toHaveBeenCalledWith({
+        data: expect.objectContaining({
+          email: 'foo@bar',
+          firstName: 'foo',
+          lastName: 'bar',
+          password: expect.any(String),
+          role: 'USER',
+          tokenSecret: expect.any(String),
+        }),
+      });
+
+      expect(res.json).toHaveBeenCalledWith({
+        email: mockedUser.email,
+        firstName: mockedUser.firstName,
+        lastName: mockedUser.lastName,
+        role: mockedUser.role,
+      });
+    });
+  });
+
+  describe('given payload with already used email', () => {
+    test('should call next with error', async () => {
+      prismaMock.user.create.mockRejectedValue(new Error());
+
+      const req = getMockReq();
+      const { res, next } = getMockRes();
+
+      await signUp(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(
+        new ResourceExistsError('Email already used')
+      );
+    });
+  });
+});
