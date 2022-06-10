1f8ecdc72ee269ca2bdb6fe4899426790da8d318
Nekiro <nekiro.dev@gmail.com>
2022-06-10 17:09:09 +0200

    Add missing sign out test

---
+++ __tests__/auth/sign-out.test.ts
@@ -0,0 +1,36 @@
+import { prismaMock } from '../../src/singleton';
+import { getMockReq, getMockRes } from '@jest-mock/express';
+import { signOut } from '../../src/handlers/auth.handler';
+import * as crypt from '../../src/util/crypt';
+
+describe('Sign-out', () => {
+  describe('given valid user', () => {
+    test('should call update on user', async () => {
+      const mockedUser: any = {
+        id: 'foo',
+      };
+
+      const generateSaltSpy = jest.spyOn(crypt, 'generateSalt');
+
+      prismaMock.user.update.mockResolvedValue(mockedUser);
+
+      const req = getMockReq({ user: mockedUser });
+      const { res, next } = getMockRes();
+
+      await signOut(req, res, next);
+
+      expect(generateSaltSpy).toHaveBeenCalledWith(6);
+
+      expect(prismaMock.user.update).toHaveBeenCalledWith({
+        where: { id: mockedUser.id },
+        data: {
+          tokenSecret: expect.any(String),
+        },
+      });
+
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Logged out succesfully',
+      });
+    });
+  });
+});
