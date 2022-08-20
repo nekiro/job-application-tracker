256bc03ce78f844a41c4530c234cc3d1002e300d
Marcin Jałocha <nekiro.dev@gmail.com>
2022-08-21 01:51:02 +0200

    Add env variables to Jest

--- src/singleton.ts
+++ src/singleton.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
 
-process.env.SKIP_AUTH = false as any;
+process.env = {
+  SKIP_AUTH: 'false',
+  REFRESH_TOKEN_SECRET: 'x',
+  ACCESS_TOKEN_SECRET: 'y',
+  REFRESH_TOKEN_LIFETIME: '86400',
+  ACCESS_TOKEN_LIFETIME: '600',
+};
 
 import prisma from './prisma';
 
